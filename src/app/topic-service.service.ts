deb77cf80d1d5360dcef68dd2e2eb27aac70cddb
adam.metal <adam.metal@codecool.com>
2022-10-03 19:29:38 +0200

    topic service

--- src/app/topic-service.service.ts
+++ src/app/topic-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { v4 as uuid } from 'uuid';
 import { ITopic } from './ITopic';
 
 @Injectable({
@@ -9,16 +10,56 @@ export class TopicServiceService {
   constructor() {}
 
   getTopics(): Observable<ITopic[]> {
-    return of([]);
+    const raw = localStorage.getItem('topics') ?? '[]';
+    const topics = <ITopic[]>JSON.parse(raw);
+    return of(topics);
   }
 
-  getTopicById(id: string): Observable<ITopic> {
-    return null;
+  getTopicById(id: string): Observable<ITopic | undefined> {
+    const raw = localStorage.getItem('topics') ?? '[]';
+    const topics = <ITopic[]>JSON.parse(raw);
+    const topic = topics.find((topic) => topic.id === id);
+    return of(topic);
   }
 
-  addTopic(t: ITopic): Observable<ITopic> {}
+  addTopic(topic: Omit<ITopic, 'id'>): Observable<ITopic> {
+    const raw = localStorage.getItem('topics') ?? '[]';
+    const topics = <ITopic[]>JSON.parse(raw);
+    const saved = { ...topic, id: uuid() };
+    topics.push(saved);
 
-  removeTopic(id: string): void {}
+    localStorage.setItem('topics', JSON.stringify(topics));
 
-  updateTopic(id: string, topic: Partial<ITopic>) {}
+    return of(saved);
+  }
+
+  removeTopic(id: string): void {
+    const raw = localStorage.getItem('topics') ?? '[]';
+    const topics = <ITopic[]>JSON.parse(raw);
+
+    const index = topics.findIndex((topic) => topic.id === id);
+    if (index !== -1) {
+      topics.splice(index, 1);
+    }
+
+    localStorage.setItem('topics', JSON.stringify(topics));
+  }
+
+  updateTopic(id: string, updated: Partial<ITopic>) {
+    const raw = localStorage.getItem('topics') ?? '[]';
+    let topics = <ITopic[]>JSON.parse(raw);
+
+    topics = topics.map((topic) => {
+      if (topic.id !== id) {
+        return topic;
+      }
+
+      return {
+        ...topic,
+        ...updated,
+      };
+    });
+
+    localStorage.setItem('topics', JSON.stringify(topics));
+  }
 }
