abfc4a84c26c84522f1636f910a3c8bd5d83f8a9
adam.metal <adam.metal@codecool.com>
2022-10-03 19:09:52 +0200

    add material formgroup

--- src/app/material-form/material-form.component.ts
+++ src/app/material-form/material-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-material-form',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./material-form.component.css']
 })
 export class MaterialFormComponent implements OnInit {
+  materialForm!: FormGroup;
+
+  label?: string = "";
+  link?: string = "";
+  description?: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+    this.materialForm = new FormGroup({
+      label: new FormControl(this.label, [Validators.required]),
+      link: new FormControl(this.link, [Validators.required]),
+      description: new FormControl(this.description)
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.materialForm.valid);
+    console.log(this.materialForm.value);
   }
 
 }
